4fe08b9431ea1aeb34374be94ab048b05e92657d
vinit pal <palv20192@gmail.com>
2025-08-06 16:31:07 +0530

    Add files via upload

---
+++ QR code/QR.js
@@ -0,0 +1,19 @@
+let imgBOX = document.getElementById("imgBOX");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+
+function generateQR() {
+  if (qrText.value.length > 0) {
+    qrImage.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      qrText.value;
+
+    imgBOX.classList.add("show-img");
+    // qrText.classList.remove("error");
+  } else {
+    qrText.classList.add("error");
+    setTimeout(() => {
+      qrText.classList.remove("error");
+    }, 1000);
+  }
+}
